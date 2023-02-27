8dc0cdc017641f5a6b1f43b78d8bc12ac2fbe049
sahilshirsekar <120778958+sahilshirsekar@users.noreply.github.com>
2023-02-27 18:24:41 +0530

    Add files via upload

---
+++ funcn.py
@@ -0,0 +1,8 @@
+def make_shirt(size = 'L', text = 'I love Python'):
+    print(f"The size of your shirt is {size} and the text you want to print is '{text}'.")
+
+# make_shirt()
+# make_shirt(size="M",)
+# make_shirt(size="XXL",text="Python sucks")
+
+
