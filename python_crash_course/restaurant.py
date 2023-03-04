e9f2286aa69d9ceac833928fb62174e969390971
sahilshirsekar <120778958+sahilshirsekar@users.noreply.github.com>
2023-03-04 12:13:18 +0530

    restaurant class

--- python_crash_course/restaurant.py
+++ python_crash_course/restaurant.py
@@ -2,6 +2,7 @@ class Restaurant:
     def __init__(self, restaurant_name, cuisine_type):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print(f"Restaurant's name is {self.restaurant_name}.")
@@ -10,6 +11,24 @@ class Restaurant:
     def open_restaurant(self):
         print(f"{self.restaurant_name} is open!!")
 
+    def number_of_served(self):
+        print(f"Today, we served {self.number_served} customers.")
+
+    def set_number_served(self, number):
+        self.number_served = number
+
+    def increment_number_served(self, day_of_busines):
+        self.number_served += day_of_busines
+
 restaurant = Restaurant("Family-Friendly", "Indian")
 restaurant.describe_restaurant()
 restaurant.open_restaurant()
+
+restaurant.number_served = 45
+restaurant.number_of_served()
+
+restaurant.set_number_served(60)
+restaurant.number_of_served()
+
+restaurant.increment_number_served(30)
+restaurant.number_of_served()
